af8353b362a826c51c1999877dd0588ef68240c8
elast27 <laste@lafayette.edu>
2017-04-27 10:22:00 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class Graph here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Graph
 {
     Vertex[] v; //Set of vertices in the graph
